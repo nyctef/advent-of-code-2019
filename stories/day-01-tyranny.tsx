6cabfe3a56cbc48c3ae8c76a9a74f05ff39f988b
Mark Jordan <mark.jordan@red-gate.com>
2019-12-04 16:02:29 +0000

    day 1 part 2

--- stories/day-01-tyranny.tsx
+++ stories/day-01-tyranny.tsx
@@ -14,7 +14,6 @@ storiesOf("day 01", module)
       const nums = lines.map(x => parseInt(x));
       const fuels = nums.map(x => Math.floor(x / 3) - 2);
 
-      console.log({ lines, nums, fuels });
       return fuels.reduce((a, b) => a + b, 0);
     }, [input]);
 
@@ -29,5 +28,52 @@ storiesOf("day 01", module)
     );
   })
   .add("part 2", () => {
-    return <></>;
+    const [input, setInput] = useState<string>("");
+
+    const fuelForMass = (mass: number) => Math.max(Math.floor(mass / 3) - 2, 0);
+
+    const fuelForMass2 = (mass: number) => {
+      let fuelMass = fuelForMass(mass);
+      let additionalFuel = fuelForMass(fuelMass);
+      while (true) {
+        if (additionalFuel > 0) {
+          //   result.push(
+          //     `Fuel mass ${mass} requires additional fuel ${additionalFuel}`
+          //   );
+          fuelMass += additionalFuel;
+        } else {
+          //   result.push(`Fuel mass ${mass} can be launched`);
+          break;
+        }
+        additionalFuel = fuelForMass(additionalFuel);
+      }
+      return fuelMass;
+    };
+
+    const answer = useMemo(() => {
+      if (!input) {
+        return ["set input"];
+      }
+
+      const lines = input.split(/[\r\n ]+/g);
+      const nums = lines.map(x => parseInt(x));
+      const fuels = nums.map(fuelForMass2);
+      let mass = fuels.reduce((a, b) => a + b, 0);
+
+      //   const result: string[] = [];
+
+      //   return result;
+
+      return mass;
+    }, [input]);
+
+    return (
+      <>
+        <div>
+          Problem input:{" "}
+          <input value={input} onChange={e => setInput(e.target.value)} />
+        </div>
+        <div>Answer: {answer}</div>
+      </>
+    );
   });
