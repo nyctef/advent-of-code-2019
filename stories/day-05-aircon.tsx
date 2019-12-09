f67df0f53c6c0466dbc2c037e80e059b882bb786
Nyctef <nyctef@nyctef.com>
2019-12-09 14:45:29 +0000

    day 5 part 2: more intcode

--- stories/day-05-aircon.tsx
+++ stories/day-05-aircon.tsx
@@ -18,6 +18,10 @@ const OP_ADD = 1;
 const OP_MULT = 2;
 const OP_INPUT = 3;
 const OP_OUTPUT = 4;
+const OP_JNZ = 5;
+const OP_JZ = 6;
+const OP_LT = 7;
+const OP_EQ = 8;
 const OP_HALT = 99;
 
 const PARAM_POSITION = 0;
@@ -73,7 +77,7 @@ const intcode = (memory: number[], input: number[]) => {
       }
 
       case OP_INPUT: {
-        const value = input.unshift();
+        const value = input.shift() || 0;
         const addr = memory[ip + 1];
         log.push(`Setting ${addr} to ${value}`);
         memory[addr] = value;
@@ -88,13 +92,61 @@ const intcode = (memory: number[], input: number[]) => {
         break;
       }
 
+      case OP_JNZ: {
+        const value = fetch(memory, ip + 1, instruction.p1mode);
+        const target = fetch(memory, ip + 2, instruction.p2mode);
+        if (value) {
+          log.push("setting ip to " + target);
+          ip = target;
+        } else {
+          ip += 3;
+        }
+        break;
+      }
+      case OP_JZ: {
+        const value = fetch(memory, ip + 1, instruction.p1mode);
+        const target = fetch(memory, ip + 2, instruction.p2mode);
+        if (!value) {
+          log.push("setting ip to " + target);
+          ip = target;
+        } else {
+          ip += 3;
+        }
+        break;
+      }
+
+      case OP_LT: {
+        const v1 = fetch(memory, ip + 1, instruction.p1mode);
+        const v2 = fetch(memory, ip + 2, instruction.p2mode);
+        const addr = memory[ip + 3];
+
+        memory[addr] = v1 < v2 ? 1 : 0;
+        ip += 4;
+        break;
+      }
+      case OP_EQ: {
+        const v1 = fetch(memory, ip + 1, instruction.p1mode);
+        const v2 = fetch(memory, ip + 2, instruction.p2mode);
+        const addr = memory[ip + 3];
+
+        log.push(
+          `Comparing ${v1} and ${v2}: ${
+            v1 === v2 ? "equal" : "not equal"
+          } . Setting value at ${addr}`
+        );
+
+        memory[addr] = v1 === v2 ? 1 : 0;
+        ip += 4;
+        break;
+      }
+
       case OP_HALT: {
         log.push("99: halt");
         done = true;
         break;
       }
       default: {
-        log.push(instruction + ": unknown");
+        log.push(instruction.opcode + ": unknown");
         done = true;
         break;
       }
@@ -130,8 +182,17 @@ storiesOf("day 05", module)
   })
   .add("part 2", () => {
     const [input, setInput] = usePersistedState("day 5 part 2", "");
+    const [machineInput, setMachineInput] = usePersistedState(
+      "day 5 part 2 input",
+      "5"
+    );
 
-    let answer = 0;
+    const memory = input
+      .trim()
+      .split(",")
+      .map(x => parseInt(x));
+
+    const { log } = intcode(memory, [parseInt(machineInput)]);
 
     return (
       <>
@@ -139,7 +200,17 @@ storiesOf("day 05", module)
           Problem input:{" "}
           <input value={input} onChange={e => setInput(e.target.value)} />
         </div>
-        <div>Answer: {answer}</div>
+        <div>
+          Machine input input:{" "}
+          <input
+            value={machineInput}
+            onChange={e => setMachineInput(e.target.value)}
+          />
+        </div>
+        <div>Answer: {JSON.stringify(memory)} </div>
+        <div>
+          Log: <pre>{log.map(x => JSON.stringify(x)).join("\n")}</pre>
+        </div>
       </>
     );
   });
